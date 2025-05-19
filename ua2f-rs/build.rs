06171162abc637437c2cc00c840ccfc0bd025291
AyaSanae <2294760573@qq.com>
2025-05-19 16:51:50 +0800

    aarch64: 添加链接库

--- ua2f-rs/build.rs
+++ ua2f-rs/build.rs
@@ -6,6 +6,17 @@ use anyhow::{Context as _, anyhow};
 use aya_build::cargo_metadata;
 
 fn main() -> anyhow::Result<()> {
+    #[cfg(target_arch = "aarch64")]
+    {
+        // libxdp and libbpf are required for linking.
+        // You should set `rustc-link-search` to specify the search path for these libraries.
+        println!("cargo:rustc-link-search=/usr/local/lib");
+        println!("cargo:rustc-link-lib=xdp");
+        println!("cargo:rustc-link-lib=bpf");
+        println!("cargo:rustc-link-lib=elf");
+        println!("cargo:rustc-link-lib=z");
+    }
+
     let cargo_metadata::Metadata { packages, .. } = cargo_metadata::MetadataCommand::new()
         .no_deps()
         .exec()
